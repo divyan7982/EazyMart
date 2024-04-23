d05b7cd8e3cae6a4eec6592f34d8b9851c06e904
divyan7982 <divyanshuchoubey72@gmail.com>
2024-04-23 08:57:34 +0530

    first commit

---
+++ front-end/src/database.js
@@ -0,0 +1,136 @@
+const db ={
+    products :[
+        {
+            id:1,
+            name :'Sony WX-5',
+            price: 100.75,
+            category: 'Headphones',
+            rating:3,
+            color:'red',
+            size : 'S',
+            details : {
+                product :"",
+                warranty : "",
+                merchant:""
+            },
+            image:'product-1-square',
+            images :['product-1-square','product-1-square','product-1-square']
+        },
+        {
+            id:2,
+            name :'Apple Watch 2',
+            price: 500.75,
+            category: 'SmartWatch',
+            rating:4,
+            color:'black',
+            size : '',
+            details : {
+                product :"",
+                warranty : "",
+                merchant:""
+            },
+            image:'product-2-square',
+            images :['product-2-square','product-2-square','product-2-square']
+        },
+        {
+            id:3,
+            name :'Apple iPhone 11',
+            price: 799.75,
+            category: 'Mobile',
+            rating:4,
+            color:'black',
+            size : '',
+            details : {
+                product :"",
+                warranty : "",
+                merchant:""
+            },
+            image:'product-3-square',
+            images :['product-3-square','product-3-square','product-3-square']
+        },
+        {
+            id:4,
+            name :'DSL-R Camera',
+            price: 1000.80,
+            category: 'Camera',
+            rating:5,
+            color:'black',
+            size : '',
+            details : {
+                product :"",
+                warranty : "",
+                merchant:""
+            },
+            image:'product-4-square',
+            images :['product-4-square','product-4-square','product-4-square']
+        },
+        {
+            id:5,
+            name :'Apple I-Note',
+            price: 1200.80,
+            category: 'I-PAD',
+            rating:5,
+            color:'White',
+            size : '',
+            details : {
+                product :"",
+                warranty : "",
+                merchant:""
+            },
+            image:'product-5-square',
+            images :['product-5-square','product-5-square','product-5-square']
+        },
+        {
+            id:6,
+            name :'Drone',
+            price: 1500.00,
+            category: 'Drone',
+            rating:4,
+            color:'White',
+            size : '',
+            details : {
+                product :"",
+                warranty : "",
+                merchant:""
+            },
+            image:'product-6-square',
+            images :['product-6-square','product-6-square','product-6-square']
+        },
+        {
+            id:7,
+            name :'Dell Laptop',
+            price: 2000.00,
+            category: 'Laptop',
+            rating:5,
+            color:'White',
+            size : '',
+            details : {
+                product :"",
+                warranty : "",
+                merchant:""
+            },
+            image:'product-7-square',
+            images :['product-7-square','product-7-square','product-7-square']
+        },
+        {
+            id:8,
+            name :'Motorola G-XR',
+            price: 856.00,
+            category: 'Mobile',
+            rating:4,
+            color:'White',
+            size : '',
+            details : {
+                product :"",
+                warranty : "",
+                merchant:""
+            },
+            image:'product-8-square',
+            images :['product-8-square','product-8-square','product-8-square']
+        }
+
+    ]
+}
+
+
+export default db
